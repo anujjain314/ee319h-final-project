8786d2a48cb93f8308ec8239e165f007321f6aaf
George Koussa <georgekoussa@utexas.edu>
2021-05-01 19:54:15 -0500

    SlidePot Class

--- SlidePot.cpp
+++ SlidePot.cpp
@@ -13,8 +13,26 @@
 // Input: none
 // Output: none
 // measures from PD2, analog channel 5
-void ADC_Init(void){ 
-// lab 8
+void ADC_Init(uint32_t sac){ 
+  SYSCTL_RCGCADC_R |= 0x0001;   // 1) activate ADC0
+  SYSCTL_RCGCGPIO_R |= 0x08;    // 2) activate clock for Port D
+	__nop();
+	__nop();
+  GPIO_PORTD_DIR_R &= ~0x04;    // 4) make PD2 input
+  GPIO_PORTD_AFSEL_R |= 0x04;   // 5) enable alternate function on PD2
+  GPIO_PORTD_DEN_R &= ~0x04;    // 6) disable digital I/O on PD2
+  GPIO_PORTD_AMSEL_R |= 0x04;   // 7) enable analog functionality on PD2
+//  ADC0_PC_R &= ~0xF;
+  ADC0_PC_R |= 0x1;             // 8) configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x0123;        // 9) Sequencer 3 is highest priority
+  ADC0_ACTSS_R &= ~0x0008;      // 10) disable sample sequencer 3
+  ADC0_EMUX_R &= ~0xF000;       // 11) seq3 is software trigger
+  ADC0_SSMUX3_R &= ~0x000F;
+  ADC0_SSMUX3_R += 5;           // 12) set channel
+  ADC0_SSCTL3_R = 0x0006;       // 13) no TS0 D0, yes IE0 END0
+  ADC0_IM_R &= ~0x0008;         // 14) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;       // 15) enable sample sequencer 3
+	ADC0_SAC_R = sac;
 }
 
 //------------ADCIn------------
@@ -23,37 +41,48 @@ void ADC_Init(void){
 // Output: 12-bit result of ADC conversion
 // measures from PD2, analog channel 5
 uint32_t ADC_In(void){  
-
-// lab 8
-  return 0; // remove this, replace with real code
+    uint32_t result;
+	  ADC0_PSSI_R = 0x0008;										// 1) initiate SS3
+    while((ADC0_RIS_R&0x08) == 0); 				// 2) wait for conversion done
+    result = ADC0_SSFIFO3_R&0xFFF; 				// 3) read 12-bit result
+    ADC0_ISC_R = 0x0008;                  // 4) acknowledge completion
+    return result; 
 }
 
 // constructor, invoked on creation of class
 // m and b are linear calibration coeffients 
 SlidePot::SlidePot(uint32_t m, uint32_t b){
-// lab 8
+// make slope equal to m and offset equal to b
+	slope = m;
+	offset = b;
 }
 
 void SlidePot::Save(uint32_t n){
-// lab 9
+// 1) save ADC sample into private variable
+// 2) calculate distance from ADC, save into private variable
+// 3) set semaphore flag = 1
+	data = n;
+	distance = Convert(data);
+	flag = 1;
 }
 uint32_t SlidePot::Convert(uint32_t n){
-  // lab 8
-  return 0;
+  // use calibration data to convert ADC sample to distance
+  return (slope * n ) / 4096 + offset;
 }
 
 void SlidePot::Sync(void){
-// lab 8
+		while(!flag);			// 1) wait for semaphore flag to be nonzero
+		flag = 0;						// 2) set semaphore flag to 0
 }
 
 uint32_t SlidePot::ADCsample(void){ // return ADC sample value (0 to 4095)
-
-  return 0;
+  // return last calculated ADC sample
+  return data;
 }
 
 uint32_t SlidePot::Distance(void){  // return distance value (0 to 2000), 0.001cm
-// lab 8
-  return 0;
+  // return last calculated distance in 0.01cm
+  return distance;
 }
 
 
